be38ec73dcb8bfb188da274898eabe6f6a05f9d2
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-19 16:49:10 +0300

    Is between

---
+++ src/main/java/stage3/hw1/Main.java
@@ -0,0 +1,15 @@
+package stage3.hw1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println(a >= b && a <= c || a >= c && a <= b);
+    }
+}
