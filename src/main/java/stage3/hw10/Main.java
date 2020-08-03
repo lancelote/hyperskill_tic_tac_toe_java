a6cb21747a921f5531694bdaee0e768ee99cdc17
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-08-03 21:37:49 +0300

    Homework 10

---
+++ src/main/java/stage3/hw10/Main.java
@@ -0,0 +1,9 @@
+package stage3.hw10;
+
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        System.out.printf("The input string: %s", input);
+    }
+}
