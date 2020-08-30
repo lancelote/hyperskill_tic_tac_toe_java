5a0ee32fc02713b01d9eb352011ffa34118005e2
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-08-30 13:03:27 +0300

    Template solution

--- src/main/java/stage3/hw15/Main.java
+++ src/main/java/stage3/hw15/Main.java
@@ -3,9 +3,21 @@ package stage3.hw15;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static int[][] sumNeighbors(int[][] matrix) {
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            String line = Arrays.stream(row).mapToObj(String::valueOf).collect(Collectors.joining(" "));
+            System.out.println(line);
+        }
+    }
+
+    public static int[][] readMatrix() {
         Scanner scanner = new Scanner(System.in);
         ArrayList<int[]> rawData = new ArrayList<>();
 
@@ -22,7 +34,10 @@ public class Main {
         // Convert to a faster data structure
         int[][] matrix = new int[rawData.size()][];
         matrix = rawData.toArray(matrix);
+        return matrix;
+    }
 
-        // Construct new matrix
+    public static void main(String[] args) {
+        printMatrix(sumNeighbors(readMatrix()));
     }
 }
