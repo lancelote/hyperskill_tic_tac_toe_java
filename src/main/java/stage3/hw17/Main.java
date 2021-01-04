04578f2db696200e5bc4a89567859f1f1eca6d62
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-04 23:12:26 +0300

    Homework 17

---
+++ src/main/java/stage3/hw17/Main.java
@@ -0,0 +1,18 @@
+package stage3.hw17;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int sum = a + b + c;
+
+        System.out.println(sum);
+    }
+}
+
